3fd496e933e5b5a32631d4df527fbab558870437
Javier Medel, MSc <msc.javier.medel@gmail.com>
2025-05-16 20:24:39 -0400

    fix(layout): correct HTML structure by adding missing head elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
+<html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
